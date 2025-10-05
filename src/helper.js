15c688351b97ce1f031aa5dc2a024ca36a7eb507
Acoustic Typer <melodictyper@gmail.com>
2025-10-05 11:25:24 -0700

    Job actions

---
+++ src/helper.js
@@ -0,0 +1,7 @@
+export function generateRandom (min, max) {
+  return Math.floor(Math.random() * (max - min +1) + min)
+}
+export function chance (percent) {
+  return percent >= generateRandom(1, 100)
+  
+}
